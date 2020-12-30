f88054041e1dd93d612012d01a98eb9d6cf595f5
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-12-30 23:12:59 +0500

    style: use css modules to style components

--- src/components/workflows/WorkflowPreview.tsx
+++ src/components/workflows/WorkflowPreview.tsx
@@ -1,36 +1,36 @@
 import { Workflow } from '../../model/Workflows'
 import React from 'react'
-import './WorkflowPreview.scss'
+import styles from './WorkflowPreview.module.scss'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 export default function WorkflowPreview (props: {
   workflow: Workflow
 }) {
   return (<>
-    <ol className="stages-list">
+    <ol>
       {props.workflow.stages.map((stage, i) => (
-        <li key={i} className="stage">
-          <span className="stage-duration">Duration: {stage.duration.seconds()}s</span>
+        <li key={i} className={styles.stage}>
+          <span>Duration: {stage.duration.seconds()}s</span>
 
-          <ul className="steps-list">
+          <ul className={styles.stepsList}>
             {stage.steps.map((step, i) => (
-              <li key={i} className="step">
-                <p className="step-name">{step.name}</p>
+              <li key={i} className={styles.step}>
+                <p className={styles.stepName}>{step.name}</p>
 
-                <div className="step-target">
-                  <p className="step-target-name">{step.target.label}</p>
-                  <p className="step-target-namespace">{step.target.namespace}</p>
-                  <p className="step-target-kind">{step.target.kind}</p>
+                <div>
+                  <p className={styles.targetName}>{step.target.label}</p>
+                  <p className={styles.targetNamespace}>{step.target.namespace}</p>
+                  <p className={styles.targetKind}>{step.target.kind}</p>
                 </div>
 
-                <ul className="step-parameters-list">
+                <ul>
                   {step.parameters.map((parameter, i) => (
-                    <li key={i} className="step-parameter">{parameter.name}={parameter.value}</li>
+                    <li key={i}>{parameter.name}={parameter.value}</li>
                   ))}
                 </ul>
 
                 <CopyToClipboard text={step.manifest}>
-                  <button className="step-copy-button">Copy as YAML</button>
+                  <button className={styles.copyButton}>Copy as YAML</button>
                 </CopyToClipboard>
               </li>))}
           </ul>
