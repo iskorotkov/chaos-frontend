a45611d4f8354c6e245ec21de605fdc35dc7b172
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-12-31 00:22:37 +0500

    style: paint failed and succeeded steps and stages

--- src/components/workflows/WorkflowStatus.tsx
+++ src/components/workflows/WorkflowStatus.tsx
@@ -1,6 +1,7 @@
 import { WorkflowEvent } from '../../model/WorkflowEvents'
 import React from 'react'
 import styles from './WorkflowStatus.module.scss'
+import classNames from 'classnames'
 
 export default function WorkflowStatus (props: {
   workflow: WorkflowEvent
@@ -22,17 +23,23 @@ export default function WorkflowStatus (props: {
         })}
       </ul>
 
-      <ul>
+      <ol>
         {workflow.stages.map((stage, index) => {
           return (
-            <li key={index} className={styles.stage}>
+            <li key={index} className={classNames(styles.stage, {
+              [styles.stageSucceeded]: stage.phase === 'Succeeded',
+              [styles.stageFailed]: stage.phase === 'Failed'
+            })}>
               <p className={styles.stageName}>Stage #{index} - {stage.phase}</p>
               <p className={styles.stageTime}>{formatDate(stage.startedAt)} - {formatDate(stage.finishedAt)}</p>
 
               <ul className={styles.stepsList}>
                 {stage.steps.map(step => {
                   return (
-                    <li key={step.name} className={styles.step}>
+                    <li key={step.name} className={classNames(styles.step, {
+                      [styles.stepSucceeded]: step.phase === 'Succeeded',
+                      [styles.stepFailed]: step.phase === 'Failed'
+                    })}>
                       <p className={styles.stepName}>{step.name} - {step.phase}</p>
                       <p className={styles.stepTime}>{formatDate(step.startedAt)} - {formatDate(step.finishedAt)}</p>
                       <p className={styles.stepType}>{step.type}</p>
@@ -43,7 +50,7 @@ export default function WorkflowStatus (props: {
             </li>
           )
         })}
-      </ul>
+      </ol>
     </>
   )
 }
